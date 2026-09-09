c42c6d719489db3a28887fad7d4b98b160c34025
Матвей Гусамов <gusamovmatvej@gmail.com>
2026-09-10 02:10:20 +0500

    Task3

---
+++ Tyuiu.GusamovMD.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.GusamovMD.Sprint0.Task3.V0.Lib;
+namespace Tyuiu.GusamovMD.Sprint0.Task3.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Sum(7, 7));
+            
+        }
+    }
+}
